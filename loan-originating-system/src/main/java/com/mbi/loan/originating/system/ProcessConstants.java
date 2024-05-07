5fbd644689d02a9fc87cdb074244bdfb92db7223
vivekmali123 <88703758+vivekmali123@users.noreply.github.com>
2024-05-07 13:05:33 +0530

    Delete loan-originating-system directory

--- loan-originating-system/src/main/java/com/mbi/loan/originating/system/ProcessConstants.java
+++
@@ -1,7 +0,0 @@
-package com.mbi.loan.originating.system;
-
-public class ProcessConstants {
-
-  public static final String PROCESS_DEFINITION_KEY = "loan-originating-system"; // BPMN Process ID
-
-}

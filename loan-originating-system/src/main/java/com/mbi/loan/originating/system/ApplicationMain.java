7eb04f8b33f719309268cd6c12b68bfacc4b01e4
palash-droid <63289995+palash-droid@users.noreply.github.com>
2024-05-07 13:18:37 +0530

    Add files via upload

---
+++ loan-originating-system/src/main/java/com/mbi/loan/originating/system/ApplicationMain.java
@@ -0,0 +1,45 @@
+package com.mbi.loan.originating.system;
+
+import java.util.logging.Logger;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+import java.lang.Long;
+
+@Component("ApplicationMain")
+
+public class ApplicationMain implements JavaDelegate {
+
+	private final Logger LOGGER = Logger.getLogger(ApplicationMain.class.getName());
+	 
+	 @Override
+		public void execute(DelegateExecution execution) throws Exception {
+		 
+		 
+	        // Setting the default email variables
+	        String to = "vivekmali448@gmail.com";
+	        String from = "q2292803@gmail.com";
+	        String subject = "Loan Approval";
+	       
+	        
+	        String approvalnotification =(String)execution.getVariable("approvalstatus");
+	        String text="Congrats!! Loan approved... " + "\n Please Submit hard copies of all documents...";
+	        // Sending email notification
+	       
+	        
+	       
+	        GmailSender gEmailSender = new GmailSender();
+	        boolean b = gEmailSender.sendEmail(to, from, subject, text);
+	        
+	        // Checking if email sent successfully
+	        if (b) {
+	            System.out.println("Email notification sent successfully.");
+	        } else {
+	            System.out.println("There was a problem sending the email notification.");
+	        }
+
+	      
+		 
+	 }
+}
---
+++ loan-originating-system/src/main/java/com/mbi/loan/originating/system/ProcessConstants.java
@@ -0,0 +1,7 @@
+package com.mbi.loan.originating.system;
+
+public class ProcessConstants {
+
+  public static final String PROCESS_DEFINITION_KEY = "loan-originating-system"; // BPMN Process ID
+
+}
